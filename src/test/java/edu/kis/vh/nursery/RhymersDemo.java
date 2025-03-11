02d4ed5ac2a0660eba1f4c6e17288a3a916f21d1
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2025-03-11 13:18:38 +0100

    4.2 Zmiana konwencji nazewniczej metod

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -20,7 +20,7 @@ class RhymersDemo {
             rhymers[3].countIn(rn.nextInt(20));
         
         for (int i = 0; i < rhymers.length; i++) {
-            while (!rhymers[i].callCheck())
+            while (!rhymers[i].isEmpty())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
